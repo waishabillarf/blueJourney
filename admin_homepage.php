7ea9520839e6ed5e3ef3b248e49fb5246051a1de
meikesyahra1505 <afiemeike@gmail.com>
2025-01-02 17:57:16 +0700

    admin & verifikasi

---
+++ admin_homepage.php
@@ -0,0 +1,226 @@
+<?php
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>admin</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="assets/home.css">
+</head>
+
+<body>
+    <div class="kontak">
+    <a href="https://wa.me/6285748029269?text=Assalamu%27alaikum%2C%20perkenalkan%20nama%20saya"><img src="assets/image/warill.png" alt=""></a>
+    </div>
+    <div class="nav-container">
+        <header>
+            <div class="logo">
+                <img src="assets/image/logo bener.png" alt="logo">
+            </div>
+            <nav>
+                <ul>
+                    <li><a href="verifikasi.php">Verified</a></li>
+                    <!-- <li><a href="#schedule_section">Schedule</a></li>
+                    <li><a href="#type&ticket_section">Type & ticket</a></li>
+                    <li><a href="https://wa.me/6285748029269?text=Assalamu%27alaikum%2C%20perkenalkan%20nama%20saya">Contact</a></li> -->
+                    <li class="login_button"><a href="logout_page.php">Log out</a></li>
+                </ul>
+            </nav>
+        </header>
+    </div>
+    <div class="hero-container">
+        <img src="assets/image/Kapal.png" alt="kapal">
+        <div>
+            <h1>MARI BERLAYAR</h1>
+            <h2>Bersama Kami, <br>
+                kemanapun kamu pergi</h2>
+            <p>Mari Berlayar is a ship ticketing company based <br>
+                in Surabaya, East Java. This company was <br>
+                founded in 2023 and has served the Indonesian <br>
+                people to travel to various regions in the <br>
+                archipelago via sea transportation.</p>
+            <!-- <a href="#" class="button">Get ticket</a> -->
+            <!-- <a href="#" class="button">learn more</a> -->
+        </div>
+    </div>
+    <div class="blog-container">
+        <header>
+            <h1>Types and Ticket</h1>
+            <div class="pencetan">
+                <!-- <a href="#" class="button1">Date & time</a>
+                <a href="#" class="button1">Any category</a> -->
+            </div>
+        </header>
+        <div class="blog-index" id="type&ticket_section">
+            <div class="snippet">
+                <img class="snippet-img" src="assets/image/kalimutu.jpg">
+                <div class="snippet-txt">
+                    <div class="bulan">
+                        <p>DEC</p>
+                        <h1>21</h1>
+                    </div>
+                    <div class="deskripsiKapal">
+                        <h2>KM KELIMUTU SURABAYA - SAMPIT</h5>
+                            <p> The Kelimutu Ship sailed again Thursday Evening for
+                                Surabaya to Sampit it has a passenger capacity of 1000.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="snippet">
+                <img class="snippet-img" src="assets/image/kelud.jpg">
+                <div class="snippet-txt">
+                    <div class="bulan">
+                        <p>DEC</p>
+                        <h1>24</h1>
+                    </div>
+                    <div class="deskripsiKapal">
+                        <h2>KM KELUD JAKARTA - MEDAN</h5>
+                            <p> The Kelud ship sailed again Friday evening for Tanjung Pinang,
+                                Batam and Belawan Medan. It has a passenger capacity of 2000.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="snippet">
+                <img class="snippet-img" src="assets/image/dobon.jpg">
+                <div class="snippet-txt">
+                    <div class="bulan">
+                        <p>DEC</p>
+                        <h1>29</h1>
+                    </div>
+                    <div class="deskripsiKapal">
+                        <h2>KM DOBONSOLO SURABAYA - AMBON</h5>
+                            <p>The Dobonsolo ship has four classes of passenger cabins which
+                                can accommodate 1974 people.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- <div class="pesan">
+        <button>Get ticket</button>
+    </div> -->
+
+    <!-- <div class="superhero" id="schedule_section">
+        <div class="superhero_content">
+            <div class="form">
+                <div>
+                    <label for="">Search Ticket</label>
+                    <input type="text" placeholder="blababla">
+                </div>
+                <div>
+                    <label for="">Type</label>
+                    <input type="text" placeholder="blablabla">
+                </div>
+                <div>
+                    <label for="">Time</label>
+                    <input type="text" placeholder="blablabla">
+                </div>
+                <div>
+                    <label for="">From</label>
+                    <input type="text" placeholder="blablabla">
+                </div>
+                <div>
+                    <label for="">To</label>
+                    <input type="text" placeholder="blablabla">
+                </div>
+            </div>
+            <div class="getTicket">
+                <button>Get ticket</button>
+            </div>
+
+        </div>
+    </div> -->
+    <div class="search">
+        <img src="assets/image/KAPAL 2.png" alt="kapal">
+        <div class="search_isian">
+            <h1>Make your own Ship!</h1>
+            <h2>Organize a boat departure event to tour the <br>
+                island with your friends</h2><br>
+            <br>
+            <!-- <a href="#" class="button">Create even ship</a> -->
+        </div>
+    </div>
+
+    <div class="payment">
+        <div class="isian">
+            <h1>Payment Method</h1>
+            <p>We have collaborated with various institutions, so you can pay through <br>
+                several platforms and institutions below</p>
+        </div>
+        <div class="bank">
+            <img src="assets/image/Bank BCA Logo (PNG-720p) - FileVector69.png" alt="bca">
+            <img src="assets/image/CIMB Niaga Logo (PNG720p) - Vector69Com.png" alt="cimb">
+            <img src="assets/image/Bank Mandiri Logo (PNG-720p) - FileVector69.png" alt="mandiri">
+            <img src="assets/image/BNI Bank Negara Indonesia.png" alt="bni">
+            <img src="assets/image/Bank BRI (Bank Rakyat Indonesia) Logo (PNG-720p) - FileVector69.png" alt="bri">
+        </div>
+        <div class="bank">
+            <img src="assets/image/Logo LinkAja (PNG-1080p) - FileVector69.png" alt="linkaja">
+            <img src="assets/image/Logo DANA (PNG-1080p) - FileVector69.png" alt="dana">
+            <img src="assets/image/Logo GoPay (PNG-1080p) - FileVector69.png" alt="gopay">
+            <img src="assets/image/Logo OVO (PNG-1080p) - FileVector69 - Copy.png" alt="ovo">
+        </div>
+    </div>
+    <footer class="footer-container">
+        <div class="footer-menu">
+            <section>
+                <div class="logo-container">
+                    <a href="#">
+                        <img src="assets/image/logo putih benar.png">
+                    </a>
+                </div>
+                <p>Mari Berlayar is a ship ticketing <br>
+                    company based in Surabaya, East Java. <br>
+                    This company was founded in 2023 <br>
+                    and has served the Indonesian people <br>
+                    to travel to various regions in the <br>
+                    archipelago via sea transportation. </p>
+            </section>
+            <section class="duwa">
+                <h3>Ship Event</h3>
+                <ul>
+                    <!-- <li><a href="#">Create and Set Up</a></li> -->
+                    <li><a href="#">Buy Ticket</a></li>
+                </ul>
+
+            </section>
+            <section class="tiga">
+                <h3>Mari berlayar</h3>
+                <ul>
+                    <!-- <li><a href="#">About us</a></li> -->
+                    <li><a href="#">Ticket</a></li>
+                    <li><a href="#">Types</a></li>
+                    <li><a href="#">Schedule</a></li>
+                </ul>
+            </section>
+            <section class="empat">
+                <h3>Stay in the website</h3>
+                <ul>
+                    <li><i class="fas fa-envelope fa-fw"></i> Join our mailing list to stay in the Website with our <br>
+                        newest for Event and concert li>
+                </ul>
+            </section>
+        </div>
+        <hr>
+        <div class="footer-copyright">
+            <div>
+                <small class="kopirek">&copy; Copyright © 2022 MARI BERLAYAR</small>
+            </div>
+            <div>
+                <a href="#"><i class="fab fa-facebook fa-lg"></i></a>
+                <a href="#"><i class="fab fa-twitter fa-lg"></i></a>
+                <a href="#"><i class="fab fa-instagram fa-lg"></i></a>
+                <a href="#"><i class="fab fa-google-plus fa-lg"></i></a>
+                <a href="#"><i class="fab fa-github fa-lg"></i></a>
+            </div>
+        </div>
+
+</body>
+
+</html>
---
+++ verifikasi.php
@@ -0,0 +1,106 @@
+<?php
+include 'koneksi.php'; //role Admin
+$sql = "
+        SELECT transaksi.id_transaksi, pelanggan.nama as nama_pelanggan, pelanggan.NIK, pelanggan.no_telp, 
+            jadwal.nama_kapal, jadwal.rute, jadwal.jam_berangkat, transaksi.jenis_kapal, transaksi.qty, transaksi.isVerified, jadwal.harga
+        FROM transaksi
+        JOIN pelanggan ON transaksi.id_pelanggan = pelanggan.id_pelanggan
+        JOIN jadwal ON transaksi.id_jadwal = jadwal.id_jadwal
+    ";
+$result = $connection->query($sql);
+
+if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["transactionId"])) {
+    $transactionId = $_POST["transactionId"];
+
+    $updateSql = "UPDATE transaksi SET isVerified = true WHERE id_transaksi = $transactionId";
+
+    if ($connection->query($updateSql) === TRUE) {
+        echo "Record updated successfully";
+        header("location:verifikasi.php");
+    } else {
+        echo "Error updating record: " . $connection->error;
+    }
+}
+
+if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["deleteTransaction"])) {
+    $transactionId = $_POST["transactionId"];
+
+    // Perform the delete query
+    $deleteSql = "DELETE FROM transaksi WHERE id_transaksi = $transactionId";
+
+    if ($connection->query($deleteSql) === TRUE) {
+        header("location:verifikasi.php");
+    } else {
+        echo "Error deleting record: " . $connection->error;
+    }
+}
+
+$connection->close();
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>verifikasi</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="assets/verifikasi.css">
+</head>
+
+<body>
+    <div>
+        <img src="assets/image/Mari Berlayar Biru Hori.png" alt="">
+    </div>
+    <div class="verified">
+        <table>
+            <tr>
+                <th>#</th>
+                <th>Transaction ID</th>
+                <th>Username</th>
+                <th>Jumlah Tiket</th>
+                <th>Ship Type</th>
+                <th>Total Bayar</th>
+                <th>Status</th>
+                <th>Action</th>
+            </tr>
+            <?php
+            if ($result->num_rows > 0) {
+                $number = 1;
+                while ($row = $result->fetch_assoc()) {
+            ?>
+                    <tr>
+                        <td><?php echo $number; ?></td>
+                        <td><?php echo $row['id_transaksi'] ?></td>
+                        <td><?php echo $row['nama_pelanggan'] ?></td>
+                        <td><?php echo $row['qty'] ?></td>
+                        <td><?php echo $row['jenis_kapal'] ?></td>
+                        <td><?php echo "Rp " . number_format($row['harga'] * $row['qty'], 0, ',', '.'); ?></td>
+                        <td><?php echo ($row['isVerified'] == 1) ? 'Verified' : 'No Verfified'; ?></td>
+                        <td>
+                            <form method="post">
+                                <input type="hidden" name="transactionId" value="<?php echo $row['id_transaksi']; ?>">
+                                <button type="submit">Verified</button>
+                            </form>
+                            <form method="post">
+                                <input type="hidden" name="transactionId" value="<?php echo $row['id_transaksi']; ?>">
+                                <button type="submit" name="deleteTransaction">Delete</button>
+                            </form>
+                        </td>
+                    </tr>
+            <?php
+                    $number++;
+                }
+            }
+            ?>
+        </table>
+    </div>
+    <div class="balik">
+        <a href="admin_homepage.php"><button>Back to homepage</button></a>
+        
+    </div>
+</body>
+
+</html>
